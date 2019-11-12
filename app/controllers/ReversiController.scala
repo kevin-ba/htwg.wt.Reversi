e9f6b8a14576c439d44b1be5dbbb30614368bc5c
kevin-ba <kevin.bauer2@gmx.de>
2019-11-12 23:34:18 +0100

    removed bot

--- app/controllers/ReversiController.scala
+++ app/controllers/ReversiController.scala
@@ -34,16 +34,6 @@ class ReversiController @Inject()(cc: ControllerComponents) extends AbstractCont
     Ok(views.html.reversi(gameController))
   }
 
-  def enableBot() = Action {
-    gameController.enableBot()
-    Ok(views.html.reversi(gameController))
-  }
-
-  def disableBot() = Action {
-    gameController.disableBot()
-    Ok(views.html.reversi(gameController))
-  }
-
   def save() = Action {
     gameController.save()
     Ok(views.html.reversi(gameController))
